575a436ea67ce6e170447b760c35f3a8f47a8451
Plonteras191 <plonteras.johnkristoffer@gmail.com>
2025-06-28 13:48:05 +0800

    ADD NEW SECTION

--- index.php
+++ index.php
@@ -3,15 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
-    <link rel="stylesheet" href="assets/header.css">
-    <link rel="stylesheet" href="assets/home.css">
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="assets/css/header.css" />
+  <link rel="stylesheet" href="assets/css/home.css" />
+  <link rel="stylesheet" href="assets/css/about.css" />
+  <link rel="stylesheet" href="assets/css/services.css" />
+  <link rel="stylesheet" href="assets/css/blog.css" />
+  <link rel="stylesheet" href="assets/css/contact.css" />
+  <link rel="stylesheet" href="assets/css/footer.css" />
     <title>Document</title>
 </head>
 <body class="home-body">
 
-    <?php include "components/header.html"; ?>
+  <?php include "components/header.html"; ?>
+
+  <main>
     <?php include "components/home.html"; ?>
+    <?php include "components/about.html"; ?>
+    <?php include "components/services.html"; ?>
+    <?php include "components/blog.html"; ?>
+    <?php include "components/contact.html"; ?>
+  </main>
+
+  <?php include "components/footer.html"; ?>
 
 
 </body>
