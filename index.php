2dfd49f2e908f251ab1eb6f0c2c98a4306a83890
Plonteras191 <plonteras.johnkristoffer@gmail.com>
2025-06-28 11:54:33 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/header.css">
+    <link rel="stylesheet" href="assets/home.css">
+    <title>Document</title>
+</head>
+<body class="home-body">
+
+    <?php include "components/header.html"; ?>
+    <?php include "components/home.html"; ?>
+
+
+</body>
+</html>
